39819d92df671e7bf1a63614b3bbacb307563039
timming-au <timming.au@gmail.com>
2023-05-05 15:26:44 +0800

    halfway migrating

--- src/components/canvas/Scene.tsx
+++ src/components/canvas/Scene.tsx
@@ -2,9 +2,6 @@
 import { Canvas } from '@react-three/fiber'
 import { Preload, PerspectiveCamera, PerformanceMonitor } from '@react-three/drei'
 import { Vector3 } from 'three'
-import Camera from './PanningCamera'
-import Effects from './Effects'
-import LightCube from './LightCube'
 import { useState, useEffect } from 'react'
 import React from 'react'
 import { r3f } from '@/helpers/global'
@@ -16,14 +13,9 @@ export default function Scene({ ...props }) {
   return (
     <Canvas {...props}>
       <PerformanceMonitor onIncline={() => setDpr(window.devicePixelRatio)} onDecline={() => setDpr(1)}>
-        <LightCube />
-        <ambientLight intensity={0.01} />
-        <hemisphereLight groundColor='black' intensity={0.01} />
         {/* {children} */}
         <r3f.Out />
         <Preload all />
-        <Camera />
-        <Effects />
       </PerformanceMonitor>
     </Canvas>
   )
